13ad1ad8b65c375c43bbb3d4ceaba9a2aaa11cdc
Huy Hoang <ngoc.hoang@stud.th-deg.de>
2024-09-12 10:53:51 +0200

    update: allow current folder run Regelum inside docker

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -35,8 +35,8 @@ else
                --name ros-regelum ros-regelum-rl-img
 fi
 
-cd ROOT_DIR/.git && \
-  sudo chgrp -R ${id -g -n ${whoamai}} . &&\
+cd $ROOT_DIR/.git && \
+  sudo chgrp -R $(id -g -n $(whoami)) . &&\
   sudo chmod -R g+rwX . &&\
   sudo find . -type d -exec chmod g+s '{}' + &&\
   cd ..
